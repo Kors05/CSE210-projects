c282bebbf640461c3cf8c409a20df47278ca082e
kors05 <matroinnovation@live.com>
2026-06-06 11:47:40 -0800

    Add activity classes for mindfulness project with menu navigation

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    private List<string> _unusedPrompts;
+    private List<string> _unusedQuestions;
+
+    private Random _random = new Random();
+
+    public ReflectingActivity()
+        : base(
+              "Reflection",
+              "This activity will help you reflect on times in your life when you have shown strength and resilience.")
+    {
+        _prompts = new List<string>()
+        {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
+
+        _questions = new List<string>()
+        {
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience?",
+            "What did you learn about yourself?",
+            "How can you keep this experience in mind in the future?"
+        };
+
+        _unusedPrompts = new List<string>(_prompts);
+        _unusedQuestions = new List<string>(_questions);
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        Console.WriteLine("\nConsider the following prompt:\n");
+        DisplayPrompt();
+
+        Console.WriteLine("\nWhen you have something in mind, press Enter.");
+        Console.ReadLine();
+
+        Console.WriteLine("\nNow ponder each of the following questions:");
+        ShowSpinner(5);
+
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write($"\n> {GetRandomQuestion()} ");
+            ShowSpinner(5);
+        }
+
+        DisplayEndingMessage();
+    }
+
+    public string GetRandomPrompt()
+    {
+        if (_unusedPrompts.Count == 0)
+        {
+            _unusedPrompts = new List<string>(_prompts);
+        }
+
+        int index = _random.Next(_unusedPrompts.Count);
+        string prompt = _unusedPrompts[index];
+        _unusedPrompts.RemoveAt(index);
+
+        return prompt;
+    }
+
+    public string GetRandomQuestion()
+    {
+        if (_unusedQuestions.Count == 0)
+        {
+            _unusedQuestions = new List<string>(_questions);
+        }
+
+        int index = _random.Next(_unusedQuestions.Count);
+        string question = _unusedQuestions[index];
+        _unusedQuestions.RemoveAt(index);
+
+        return question;
+    }
+
+    public void DisplayPrompt()
+    {
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
+    }
+}
